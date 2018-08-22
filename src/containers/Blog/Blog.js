f2bf97cd9838e125b8868257e22499101ae16de6
aimeelynnramirez <aimeelynnramirez@gmail.com>
2018-08-22 02:19:27 -0400

    adding the 404 handlers and the async components

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -3,26 +3,33 @@ import React, { Component } from 'react';
 import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 
 import './Blog.css';
-
 import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost';
+import asyncComponent from '../../hoc/asyncComponent';
+// import NewPost from './NewPost/NewPost';
+
+const AsyncNewPost = asyncComponent(() => {
+    return import('./NewPost/NewPost');
+});
 
 class Blog extends Component {
+    state = {
+        auth: true
+    }
+
     render () {
         return (
             <div className="Blog">
                 <header>
                     <nav>
                         <ul>
-                            <li><NavLink 
-                            to="/posts/"
-                            exact
-                            style={{
-                            color:'red',
-                            textDecoration:'underline'
-
-                            }}
-                           className ="active">Posts</NavLink></li>
+                            <li><NavLink
+                                to="/posts/"
+                                exact
+                                activeClassName="my-active"
+                                activeStyle={{
+                                    color: '#fa923f',
+                                    textDecoration: 'underline'
+                                }}>Posts</NavLink></li>
                             <li><NavLink to={{
                                 pathname: '/new-post',
                                 hash: '#submit',
@@ -33,15 +40,13 @@ class Blog extends Component {
                 </header>
                 {/* <Route path="/" exact render={() => <h1>Home</h1>} />
                 <Route path="/" render={() => <h1>Home 2</h1>} /> */}
-            <Switch>
-                <Route 
-                    path="/new-post" 
-                    component={NewPost} />
-                 <Route 
-                    path="/posts/" 
-                    component={Posts} />
-                <Redirect from="/" to= "/posts" />
-                 </Switch>
+                <Switch>
+                    {this.state.auth ? <Route path="/new-post" component={AsyncNewPost} /> : null}
+                    <Route path="/posts" component={Posts} />
+                    <Route render={() => <h1>Not found</h1>}/>
+                    {/* <Redirect from="/" to="/posts" /> */}
+                    {/* <Route path="/" component={Posts} /> */}
+                </Switch>
             </div>
         );
     }
