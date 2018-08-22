20f0650cbde20d86d70335ef0ec017e3ff889af6
aimeelynnramirez <aimeelynnramirez@gmail.com>
2018-08-22 00:19:19 -0400

    adding dynamic rendering post in the api called with the right id called

--- src/containers/Blog/FullPost/FullPost.js
+++ src/containers/Blog/FullPost/FullPost.js
@@ -10,9 +10,9 @@ class FullPost extends Component {
 
     componentDidMount () {
         console.log(this.props);
-        if ( this.props.id ) {
+        if ( this.props.match.params.id ) {
             if ( !this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id) ) {
-                axios.get( '/posts/' + this.props.id )
+                axios.get( '/posts/' + this.props.match.params.id )
                     .then( response => {
                         // console.log(response);
                         this.setState( { loadedPost: response.data } );
