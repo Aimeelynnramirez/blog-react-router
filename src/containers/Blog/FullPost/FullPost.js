a8ff5e747af0cce3093f3a7247387ad63ead9e4f
aimeelynnramirez <aimeelynnramirez@gmail.com>
2018-08-22 01:08:40 -0400

    now you can select each post with now every click

--- src/containers/Blog/FullPost/FullPost.js
+++ src/containers/Blog/FullPost/FullPost.js
@@ -10,8 +10,14 @@ class FullPost extends Component {
 
     componentDidMount () {
         console.log(this.props);
+      
+    }
+    componentDidUpdate(){
+        this.loadData();
+    }
+    loadData(){
         if ( this.props.match.params.id ) {
-            if ( !this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id) ) {
+            if ( !this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id != this.props.match.params.id) ) {
                 axios.get( '/posts/' + this.props.match.params.id )
                     .then( response => {
                         // console.log(response);
@@ -20,17 +26,16 @@ class FullPost extends Component {
             }
         }
     }
-
     deletePostHandler = () => {
-        axios.delete('/posts/' + this.props.id)
+        axios.delete('/posts/' + this.props.match.params.id)
             .then(response => {
                 console.log(response);
             });
     }
-
+    
     render () {
         let post = <p style={{ textAlign: 'center' }}>Please select a Post!</p>;
-        if ( this.props.id ) {
+        if ( this.props.match.params.id ) {
             post = <p style={{ textAlign: 'center' }}>Loading...!</p>;
         }
         if ( this.state.loadedPost ) {
